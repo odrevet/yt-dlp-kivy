6537c08ba933ac61d4ca03ec7581b49e8367de2d
Olivier Drevet <odrevet@gmail.com>
2020-06-27 19:32:22 +0200

    Log color

--- src/logger.py
+++ src/logger.py
@@ -2,13 +2,13 @@ class YdlLogger(object):
    log = ''
 
    def debug(self, msg):
-      self.log += msg + "\n"
+      self.log += f"{msg}\n"
 
    def warning(self, msg):
-      self.log += msg + "\n"
+      self.log += f"[color=ffff00]{msg}[/color]\n"
 
    def error(self, msg):
-      self.log += msg + "\n"
+      self.log += f"[color=ff0000]{msg}[/color]\n"
 
 def ydl_progress_hook(d):
    if d['status'] == 'finished':
