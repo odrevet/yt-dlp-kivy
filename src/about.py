d88bcc3029089e83717551949583af5057d5c39c
Olivier Drevet <odrevet@gmail.com>
2020-06-28 15:52:04 +0200

    layout. about text

--- src/about.py
+++ src/about.py
@@ -7,7 +7,11 @@ from kivy.uix.popup import Popup
 import pkg_resources
 
 class AboutPopup(Popup):
-   about_text = StringProperty(f'''Youtube-Dl Kivy Version 0.0.1 - 2018 - 2020 Olivier Drevet Released Under the GPL-V3 License
-Youtube-dl Version {pkg_resources.get_distribution('Youtube-dl').version}   Released Under the Unlicense License
-Kivy Version {kivy. __version__} Released Under the MIT License
-Python Version {sys.version} {sys.version_info}''')
+   about_text = StringProperty(f'''*Youtube-Dl Kivy Version 0.0.1 - 2018 - 2020 Olivier Drevet 
+Released Under the GPL-V3 License
+
+* Youtube-dl Version {pkg_resources.get_distribution('Youtube-dl').version} Unlicense License
+
+* Kivy Version {kivy. __version__} MIT License
+
+* Python Version {sys.version} {sys.version_info}''')
