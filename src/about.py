9570e4d1bfc8e21766583b72fb8d9abe13fc71b8
Olivier Drevet <odrevet@gmail.com>
2020-06-28 12:01:57 +0200

    about dialog print licenses and youtube-dl version

--- src/about.py
+++ src/about.py
@@ -4,8 +4,10 @@ import kivy
 import youtube_dl
 from kivy.properties import StringProperty
 from kivy.uix.popup import Popup
+import pkg_resources
 
 class AboutPopup(Popup):
-   about_text = StringProperty(f'''Youtube-Dl Kivy Version 0.0.1 - 2018 - 2020 Olivier Drevet
-Kivy Version {kivy. __version__}
+   about_text = StringProperty(f'''Youtube-Dl Kivy Version 0.0.1 - 2018 - 2020 Olivier Drevet Released Under the GPL-V3 License
+Youtube-dl Version {pkg_resources.get_distribution('Youtube-dl').version}   Released Under the Unlicense License
+Kivy Version {kivy. __version__} Released Under the MIT License
 Python Version {sys.version} {sys.version_info}''')
