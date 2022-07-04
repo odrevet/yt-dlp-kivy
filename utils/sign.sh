2679f75f1f9ae22507c5df51c285298e94772be5
DREVET Olivier <odrevet@gmail.com>
2022-07-04 22:00:34 +0200

    Update sign.sh

--- utils/sign.sh
+++ utils/sign.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source utils/config.sh
+
 $BUILD_TOOLS_DIR/zipalign -v -p 4 $BIN/$APP_NAME-$APP_VERSION-$TARGET_ARCH-release-unsigned.apk $BIN/$APP_NAME-aligned-unsigned.apk
 $BUILD_TOOLS_DIR/apksigner sign --ks mykeystore.keystore --out $BIN/$APP_NAME.apk $BIN/$APP_NAME-aligned-unsigned.apk
