2b698604dc955e160ac81e81ccce92ec140cb220
odrevet <odrevet@gmail.com>
2023-05-01 00:10:28 +0200

    pass password to sign.sh

--- utils/sign.sh
+++ utils/sign.sh
@@ -3,4 +3,4 @@
 source utils/config.sh
 
 $BUILD_TOOLS_DIR/zipalign -v -p 4 $BIN/$APP_NAME-$APP_VERSION-$TARGET_ARCH-release-unsigned.apk $BIN/$APP_NAME-aligned-unsigned.apk
-$BUILD_TOOLS_DIR/apksigner sign --ks mykeystore.keystore --out $BIN/$APP_NAME.apk $BIN/$APP_NAME-aligned-unsigned.apk
+$BUILD_TOOLS_DIR/apksigner sign --ks mykeystore.keystore --ks-pass pass:123456 --out $BIN/$APP_NAME.apk $BIN/$APP_NAME-aligned-unsigned.apk
