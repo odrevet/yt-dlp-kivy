5987b3b9ba866c8f570b311a5f6f6e9d30f26ad9
odrevet <odrevet@gmail.com>
2023-05-09 09:42:56 +0200

    config.sh read variable from buildozer.spec

--- utils/config.sh
+++ utils/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Read the buildozer.spec file and extract version and package.domain
+APP_VERSION=$(grep "version =" buildozer.spec | awk -F "=" '{print $2}' | tr -d '[:space:]')
+APP_NAME=$(grep "package.name =" buildozer.spec | awk -F "=" '{print $2}' | tr -d '[:space:]')
+
 BIN='bin'
-APP_NAME='yt_dlp_kivy'
 ANDROID_SDK_DIR=~/.buildozer/android/platform/android-sdk
-PACKAGE=fr.odrevet.yt_dlp_kivy
+PACKAGE=fr.odrevet.$APP_NAME
 BUILD_TOOLS_VERSION='34.0.0-rc3'
 BUILD_TOOLS_DIR=$ANDROID_SDK_DIR/build-tools/$BUILD_TOOLS_VERSION
-APP_VERSION=0.1.0
 TARGET_ARCH=arm64-v8a_armeabi-v7a
