26849c6dbe2cfaa249970d4d1c6cc0844a71e9b9
odrevet <odrevet@gmail.com>
2025-07-16 20:39:45 +0200

    log line return

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ class YdlLogger(object):
         self.lock = lock
 
     def debug(self, msg):
-        self.data["log"] += "{msg}\n"
+        self.data["log"] += f"{msg}\n"
         
     def warning(self, msg):
         with self.lock:
