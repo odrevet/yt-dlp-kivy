4d1e13972939512c428b4ab25c557687def2712a
odrevet <odrevet@gmail.com>
2025-08-16 08:38:42 +0200

    remove logger thread lock

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,12 @@
-import threading
-
 class YdlLogger(object):
     def __init__(self, download, download_id):
         self.download = download
-        self.lock = threading.Lock()
     
     def debug(self, msg):
-        with self.lock:
-            self.download["log"] += f"{msg}\n"
+        self.download["log"] += f"{msg}\n"
         
     def warning(self, msg):
-        with self.lock:
-            self.download["log"] += f"[color=ffff00]{msg}[/color]\n"
+        self.download["log"] += f"[color=ffff00]{msg}[/color]\n"
     
     def error(self, msg):
-        with self.lock:
-            self.download["log"] += f"[color=ff0000]{msg}[/color]\n"
+        self.download["log"] += f"[color=ff0000]{msg}[/color]\n"
